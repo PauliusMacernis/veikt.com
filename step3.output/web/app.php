d0ec9011d0d66a5913ac99173c7654b84efa5b10
Paulius Mačernis <sugalvojau@gmail.com>
2016-07-28 17:13:46 -0400

    S3 installed. Testing.

--- step3.output/web/app.php
+++ step3.output/web/app.php
@@ -1,61 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="">
-	<meta name="author"      content="Sergey Pozhilov (GetTemplate.com)">
-	
-	<title>VEIKT.COM</title>
+<?php
 
-	<link rel="shortcut icon" href="assets/images/gt_favicon.png">
-	
-	<!-- Bootstrap itself -->
-	<link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+use Symfony\Component\HttpFoundation\Request;
 
-	<!-- Custom styles -->
-	<link rel="stylesheet" href="assets/css/magister.css">
+/**
+ * @var Composer\Autoload\ClassLoader
+ */
+$loader = require __DIR__.'/../app/autoload.php';
+include_once __DIR__.'/../var/bootstrap.php.cache';
 
-	<!-- Fonts -->
-	<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-	<link href='http://fonts.googleapis.com/css?family=Wire+One' rel='stylesheet' type='text/css'>
-</head>
+$kernel = new AppKernel('prod', false);
+$kernel->loadClassCache();
+//$kernel = new AppCache($kernel);
 
-<!-- use "theme-invert" class on bright backgrounds, also try "text-shadows" class -->
-<body class="theme-invert">
-
-
-<!-- Main (Home) section -->
-<section class="section" id="head">
-	<div class="container">
-
-		<div class="row">
-			<div class="col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1 text-center">	
-
-				<!-- Site Title, your name, HELLO msg, etc. -->
-				<h1 class="title">VEIKT.COM</h1>
-				<h2 class="subtitle">The Project for Us</h2>
-
-				<!-- Short introductory (optional) -->
-				<h3 class="tagline">
-					<!--Under construction-->
-				</h3>
-				
-				<!-- Nice place to describe your site in a sentence or two -->
-				<!--<p><a href="/download/" class="btn btn-default btn-lg">Download template now</a></p>-->
-	
-			</div> <!-- /col -->
-		</div> <!-- /row -->
-	
-	</div>
-</section>
-
-
-<!-- Load js libs only when the page is loaded. -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
-<script src="assets/js/modernizr.custom.72241.js"></script>
-<!-- Custom template scripts -->
-<script src="assets/js/magister.js"></script>
-</body>
-</html>
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+//Request::enableHttpMethodParameterOverride();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
