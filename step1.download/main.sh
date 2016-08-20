e6547badb82cd2b7bcecc3c006a6c2d61de1bc75
Paulius Mačernis <sugalvojau@gmail.com>
2016-08-19 23:09:29 -0400

    Testing.

--- step1.download/main.sh
+++ step1.download/main.sh
@@ -10,6 +10,7 @@ for D in $(find ./projects/* -maxdepth 0 -mindepth 0 -type d); do
 		opFile="${D}/entrance.sh"
 		if [ -f "${opFile}" ]; then
 		    echo -e "\n $(date +"%Y-%m-%d %I:%M:%S"): Started with '${opFile}'";
+		    chmod 774 $opFile
 			bash $opFile
 			echo -e "\n $(date +"%Y-%m-%d %I:%M:%S"): Finished with '${opFile}'";
 		fi
