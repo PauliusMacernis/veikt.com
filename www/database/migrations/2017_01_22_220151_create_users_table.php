80b95f972e304b60f0ad5aa40ea1c21f50ba53e2
Paulius Mačernis <sugalvojau@gmail.com>
2017-01-22 18:46:47 -0500

    Bugfix (?) production...

--- www/database/migrations/2017_01_22_220151_create_users_table.php
+++ www/database/migrations/2017_01_22_220151_create_users_table.php
@@ -15,8 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username')->unique();
-            $table->string('email')->unique();
+
+            // @TODO: "->collate('utf8_unicode_ci')" should fix the error:
+            // [PDOException] SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes
+            // MySQL 5.6.26 (current production) have problems, but 5.7.17 (dev box) seems to be ok...
+            $table->string('username')->collate('utf8_unicode_ci')->unique();
+            $table->string('email')->collate('utf8_unicode_ci')->unique();
+
             $table->string('password');
             $table->timestamps();
         });
