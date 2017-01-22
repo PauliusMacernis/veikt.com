b2956646cc78d2feb5f656a1a710b9d3c0dbbe23
Paulius Mačernis <sugalvojau@gmail.com>
2017-01-22 16:37:00 -0500

    Just another way to save notes

--- www/app/Http/Controllers/Note/NoteController.php
+++ www/app/Http/Controllers/Note/NoteController.php
@@ -2,19 +2,18 @@
 
 namespace App\Http\Controllers\Note;
 
+use App\Http\Controllers\Controller;
 use App\Models\Job;
 use App\Models\Note;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 
 class NoteController extends Controller
 {
     public function store(Request $request, Job $job)
     {
-        $note = new Note;
-        $note->body = $request->body;
-
-        $job->notes()->save($note);
+        $job->addNote(
+            new Note($request->all())
+        );
 
         return back();
 
--- www/app/Models/Job.php
+++ www/app/Models/Job.php
@@ -8,6 +8,11 @@ class Job extends Model
 {
     protected $table = 'job';
 
+    public function addNote(Note $note)
+    {
+        return $this->notes()->save($note);
+    }
+
     public function notes()
     {
         return $this->hasMany(Note::class);
