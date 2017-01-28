d90734e1e5ba2fa8bb8c247f859779271bf56f71
Paulius Mačernis <sugalvojau@gmail.com>
2017-01-28 18:49:05 -0500

    Authentication-related fixes. Comming back to Laravel's default user table.

--- www/database/migrations/2014_10_12_000000_create_users_table.php
+++ www/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,15 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-
-            // @TODO: "->collate('utf8_unicode_ci')" should fix the error on MySQL 5.6.26:
-            // [PDOException] SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes
-            // MySQL 5.6.26 (current production) have problems, but 5.7.17 (dev box) seems to be ok...
-            // Changed the collation to "utf8_unicode_ci" manually at the moment. The better solution needed for sure...
-            $table->string('username')->unique();
+            $table->string('name');
             $table->string('email')->unique();
-
             $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -35,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::drop('users');
     }
 }
